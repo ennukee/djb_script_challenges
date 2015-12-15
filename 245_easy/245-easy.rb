3e6a8892f8637e174e0b18472c49951f74f4d993
Dylan Bowers <Dylan Bowers>
2015-12-15 13:13:08 -0500

    problem description 245 easy

--- 245_easy/245-easy.rb
+++ 245_easy/245-easy.rb
@@ -1,3 +1,16 @@
+############################################
+# /r/dailyprogrammer Challenge #245 [Easy] #
+#             Date Dilemma                 #
+# 										   #
+# Short Summary: Write a program capable   #
+#   of converting any date format into the #
+#   standard YYYY-MM-DD format.			   #
+#   (i.e. 3/2/15 => 2015/03/02)			   #
+# 										   #
+# Full Problem: https://www.reddit.com/r/dailyprogrammer/comments/3wshp7/20151214_challenge_245_easy_date_dilemma/
+#  										   #
+############################################
+
 require 'date'
 vals = []
 input = "a"
