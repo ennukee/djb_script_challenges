d6d2cbf4a372081b4b0cf9f08b1e774bb8e3e7d3
enragednuke <dylan.volibowers@gmail.com>
2016-05-23 16:32:36 -0400

    268_easy tops

--- 268_easy/268_easy_client.py
+++ 268_easy/268_easy_client.py
@@ -1,3 +1,12 @@
+#
+# /r/dailyprogrammer Challenge #268 [Easy] 
+# Network and Cards: Part 1, The network
+#                        
+# https://www.reddit.com/r/dailyprogrammer/comments/4knivr/20160523_challenge_268_easy_network_and_cards/
+# 
+
+# This is the CLIENT PORTION of the Client-Server network of this problem
+
 import socket
 import sys
 
--- 268_easy/268_easy_server.py
+++ 268_easy/268_easy_server.py
@@ -1,70 +1,79 @@
-    import socket
-    import sys
-    import random
-    from _thread import *
+#
+# /r/dailyprogrammer Challenge #268 [Easy] 
+# Network and Cards: Part 1, The network
+#                        
+# https://www.reddit.com/r/dailyprogrammer/comments/4knivr/20160523_challenge_268_easy_network_and_cards/
+# 
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    users = {'root': 'root'} # server
+# This is the SERVER PORTION of the Client-Server network of this problem
 
-    try:
-      s.bind(('localhost', 8888))
-    except socket.error as msg:
-      print('Bind failed. Is port 8888 currenttly in use?')
-      sys.exit()
-     
-    s.listen(10)
-     
-    def cthread(conn):
-      username = "root"
+import socket
+import sys
+import random
+from _thread import *
 
-      while username in users.values():
-        username = 'user{}'.format(random.randrange(100000))
-      users[conn] = username
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+users = {'root': 'root'} # server
 
-      conn.send('Welcome to the server, {}. Type something and hit enter\n'.format(username).encode('utf-8'))
-      
-      try:
-        while 1:
-          data = conn.recv(1024)
-          if not data: 
-            break
+try:
+  s.bind(('localhost', 8888))
+except socket.error as msg:
+  print('Bind failed. Is port 8888 currenttly in use?')
+  sys.exit()
+ 
+s.listen(10)
+ 
+def cthread(conn):
+  username = "root"
 
-          data = str(data, 'utf-8')
-          prefix = data[0:3]
+  while username in users.values():
+    username = 'user{}'.format(random.randrange(100000))
+  users[conn] = username
 
-          if prefix == 'usr':
-            if data[4:8] == 'set ':
-
-              if len(data[8:]) > 12:
-                conn.sendall('Your desired username is too long (max 12 characters)'.encode('utf-8'))
-
-              else:
-                old, new = users[conn], data[8:]
-                if new in users:
-                  conn.sendall('Your username has already been taken.'.encode('utf-8'))
-                else:
-                  conn.sendall('Your username has been set to {}'.format(new).encode('utf-8'))
-                  users[conn] = new
-
-            elif data[4:8] == 'list':
-              online = '\n'.join([user for user in users.values() if user != 'root'])
-              conn.sendall('Users currently online:\n{}'.format(online).encode('utf-8'))
-            else:
-              conn.sendall('Your username is {}'.format(users[conn]).encode('utf-8'))
-          else:
-            conn.sendall(data.encode('utf-8'))
-      except Exception as e:
-        print('Error detected!')
-        print('{}: {}'.format(type(e).__name__, e))
-      finally:
-        users.pop(conn, None)
-        conn.close()
-        print('Client on ' + addr[0] + ':' + str(addr[1]) + ' has been disconnected')
-     
+  conn.send('Welcome to the server, {}. Type something and hit enter\n'.format(username).encode('utf-8'))
+  
+  try:
     while 1:
-      conn, addr = s.accept()
-      print('Connected with ' + addr[0] + ':' + str(addr[1]))
+      data = conn.recv(1024)
+      if not data: 
+        break
 
-      start_new_thread(cthread, (conn,))
+      data = str(data, 'utf-8')
+      prefix = data[0:3]
 
-    s.close()
+      if prefix == 'usr':
+        if data[4:8] == 'set ':
+
+          if len(data[8:]) > 12:
+            conn.sendall('Your desired username is too long (max 12 characters)'.encode('utf-8'))
+
+          else:
+            old, new = users[conn], data[8:]
+            if new in users:
+              conn.sendall('Your username has already been taken.'.encode('utf-8'))
+            else:
+              conn.sendall('Your username has been set to {}'.format(new).encode('utf-8'))
+              users[conn] = new
+
+        elif data[4:8] == 'list':
+          online = '\n'.join([user for user in users.values() if user != 'root'])
+          conn.sendall('Users currently online:\n{}'.format(online).encode('utf-8'))
+        else:
+          conn.sendall('Your username is {}'.format(users[conn]).encode('utf-8'))
+      else:
+        conn.sendall(data.encode('utf-8'))
+  except Exception as e:
+    print('Error detected!')
+    print('{}: {}'.format(type(e).__name__, e))
+  finally:
+    users.pop(conn, None)
+    conn.close()
+    print('Client on ' + addr[0] + ':' + str(addr[1]) + ' has been disconnected')
+ 
+while 1:
+  conn, addr = s.accept()
+  print('Connected with ' + addr[0] + ':' + str(addr[1]))
+
+  start_new_thread(cthread, (conn,))
+
+s.close()
