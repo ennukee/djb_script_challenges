bbc05755e4156c31bb24a0ff8355f790c121c5e4
Dylan Bowers <dylan.volibowers@gmail.com>
2016-10-19 19:51:22 -0400

    pe_p1

---
+++ project_euler/README.md
@@ -0,0 +1,3 @@
+# PLEASE NOTE
+
+If you are participating in Project Euler, **please do not** look at the code for problems you have not completed. This repository serves as a location for myself and potential employers to look over my coding abilities.
---
+++ project_euler/p1.cpp
@@ -0,0 +1,32 @@
+/*
+
+	Project Euler 
+	Problem #1
+
+	Dylan Bowers
+	Elapsed Run-time: 0.000000s
+	Result: 267333
+
+*/
+
+#include <stdio.h>
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+int p1() {
+	int sum = 0;
+	for(int i = 0; i < 1000; i++) { if(i%5==0 || i%3==0) { sum += i; } }
+	return sum;
+}
+
+int main() {
+	clock_t s, e;
+	s = clock();
+	int r = p1();
+	e = clock();
+	printf("Code executed in %fs\n", ((float)e - (float)s)/CLOCKS_PER_SEC);
+	printf("Result: %d", r);
+}
+	
