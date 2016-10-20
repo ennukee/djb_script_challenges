c2bdda42c441eafb422fd00f75d83018b3d8edf2
Dylan Bowers <dylan.volibowers@gmail.com>
2016-10-19 23:25:23 -0400

    p214 in C++

---
+++ project_euler/p214.cpp
@@ -0,0 +1,57 @@
+/*
+
+	Project Euler 
+	Problem #214
+
+	Dylan Bowers
+	Elapsed Run-time: 4.074000s
+	Result: 1677366278943
+	
+*/
+
+#include <stdio.h>
+#include <iostream>
+#include <math.h>
+#include <time.h>
+#define LIMIT 40000000
+
+using namespace std;
+
+void p214() {
+	long long unsigned int answer = 0;
+	int* all_totients = new int[LIMIT + 1];
+	for(int i = 0; i <= LIMIT; i++) { all_totients[i] = i; }
+	for(int n = 2; n <= LIMIT; n++) {
+		if(n == all_totients[n]) {
+			for(int m = n; m <= LIMIT; m += n) {
+				all_totients[m] /= n;
+				all_totients[m] *= n - 1;
+			}
+		}
+	}
+
+	cout << "-> All totients calculated" << endl;
+
+	int* tot_chain_lengths = new int[LIMIT + 1];
+	tot_chain_lengths[0] = 0;
+	tot_chain_lengths[1] = 1;
+
+	for(int n = 2; n < LIMIT; n++) {
+		tot_chain_lengths[n] = tot_chain_lengths[all_totients[n]] + 1;
+
+		if(tot_chain_lengths[n] == 25 && all_totients[n] == n-1) {
+			answer += n;
+		}
+	}
+	cout << "-> All TCLs calculated" << endl;
+
+	cout << answer << endl;
+}
+
+int main() {
+	clock_t s, e;
+	s = clock();
+	p214();
+	e = clock();
+	printf("Code executed in %fs\n", ((float)e - (float)s)/CLOCKS_PER_SEC);
+}
