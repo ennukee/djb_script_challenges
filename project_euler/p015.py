1b9e75ebd4b65546b99b152b6c1997149287ad03
Dylan Bowers <dylan.volibowers@gmail.com>
2016-11-14 03:53:04 -0500

    Create p015.py

---
+++ project_euler/p015.py
@@ -0,0 +1,22 @@
+#	
+#	Project Euler 
+#	Problem #15
+#
+#	Dylan Bowers
+#
+# https://projecteuler.net/problem=15
+#	
+
+
+import math
+def p015(n):
+	"""
+	How many strings can I made of 20 downs and 20 rights?
+
+	2n choose n
+	(2n)! / (n! + n!)
+	"""
+	return math.factorial(n * 2) // (math.factorial(n) ** 2)
+
+if __name__ == '__main__':
+	print(p015(20))
