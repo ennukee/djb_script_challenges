14002f3d96763b408d2086d0f15ca671ad19b338
Dylan Bowers <dylan.volibowers@gmail.com>
2016-11-16 13:38:45 -0500

    53

---
+++ project_euler/p053.py
@@ -0,0 +1,23 @@
+#	
+#	Project Euler 
+#	Problem #53
+#
+#	Dylan Bowers
+#
+#	https://projecteuler.net/problem=53
+# Unfinished Solution	
+#
+
+import math
+fac = math.factorial
+
+def p053():
+	count = 0
+
+	for i in range(1, 101):
+		for q in range(0, i):
+			if fac(i) / (fac(q) * fac(i - q)) > 1000000: count += 1
+	return count
+
+if __name__ == '__main__':
+	print(p053())
